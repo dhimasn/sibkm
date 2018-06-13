fc05a6c0192e929fd47dcbdeeb97f9359d9495f9
dhimasn <dhimaswift@gmail.com>
2018-06-14 02:00:58 +0700

    24

--- admin/editatt.php
+++ admin/editatt.php
@@ -35,11 +35,11 @@
 	<link href="../color/default.css" rel="stylesheet">
 	<link href="../data_tables/css/dataTables.bootstrap.css" rel="stylesheet">
 	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
- <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
- <!--[if lt IE 9]>
-   <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-   <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
- <![endif]-->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
 </head>
 <!-- koneksi  -->
 <body>
@@ -51,48 +51,48 @@
 					<div class="col-md-2">
 						<div class="site-logo">
 							<a href="index.php" class="brand">SI BEKAM</a>
-								</div>
-						  </div>
-						  <div class="col-md-10">
-						  <!-- Brand and toggle get grouped for better mobile display -->
-						  <div class="navbar-header">
-								<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#menu">
-								<i class="fa fa-bars"></i>
-								</button>
-						  </div>
-						  <!-- Collect the nav links, forms, and other content for toggling -->
-						  <div class="collapse navbar-collapse">
-							 <ul class="nav navbar-nav navbar-right">
-								<li><a href="index.php">Beranda</a></li>
-								<li class="active"><a href="masterdata.php">Master Data</a></li>	
-								<li><a href="kelolaakun.php">Kelola Akun</a></li>
-								<li><a href="bantuan.php">Bantuan</a></li>
-								<li><a> </a></li>
-								<li>
-								<div class="btn-group pull-right">
+         </div>
+         </div>
+         <div class="col-md-10">
+         <!-- Brand and toggle get grouped for better mobile display -->
+         <div class="navbar-header">
+         <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#menu">
+         <i class="fa fa-bars"></i>
+         </button>
+         </div>
+         <!-- Collect the nav links, forms, and other content for toggling -->
+         <div class="collapse navbar-collapse">
+         <ul class="nav navbar-nav navbar-right">
+         <li><a href="index.php">Beranda</a></li>
+         <li class="active"><a href="masterdata.php">Master Data</a></li>	
+         <li><a href="kelolaakun.php">Kelola Akun</a></li>
+         <li><a href="bantuan.php">Bantuan</a></li>
+         <li><a> </a></li>
+         <li>
+         <div class="btn-group pull-right">
 									<button class="btn btn-info btn-sm dropdown-toggle" data-toggle="dropdown">
-										<i class="glyphicon glyphicon-user"></i><span class="hidden-sm hidden-xs">Admin</span>
-										<span class="caret"></span>
+									<i class="glyphicon glyphicon-user"></i><span class="hidden-sm hidden-xs">Admin</span>
+									<span class="caret"></span>
 									</button>
 									<ul class="dropdown-menu">
-										<li><a href="#"><?php echo ($_SESSION['nama']); ?></a></li>
-										<li class="divider"></li>
-										<li><a href="logout.php">Logout</a></li>
+									<li><a href="#"><?php echo ($_SESSION['nama']); ?></a></li>
+									<li class="divider"></li>
+									<li><a href="logout.php">Logout</a></li>
 									</ul>
-        </div>
-        </li>
-        </ul>
-        </div>
-        <!-- /.Navbar-collapse -->
-        </div>
-        </div>
-       </div>
-      <!-- /.container -->
-     </nav>
-    </div>
-   <!-- /Navigation -->
- <p>
- <p>
+									</div>
+									</li>
+									</ul>
+									</div>
+									<!-- /.Navbar-collapse -->
+									</div>
+									</div>
+								 </div>
+								 <!-- /.container -->
+								 </nav>
+								 </div>
+							  <!-- /Navigation -->
+							  <p>
+							  <p>
 	<div class="container">
 		<div class="row">
 			<div class="col-lg-8 col-lg-offset-2">
@@ -125,8 +125,8 @@
             echo '<div class="alert alert-success">Data berhasil diubah.</div>';
            }
            ?>
-						<table class="table table-danger">
-							<form class="form-horizontal" method="post">
+						  <table class="table table-danger">
+							 <form class="form-horizontal" method="post">
 								<tr>
 								<td>ID Kepentingan Alternatif</td>
 								<td>:</td>
@@ -183,7 +183,7 @@
 </body>
 	<div class="container">
 		<div class="row">
-			<div class="text-right">
+			<div class = "text-right">
 				<b>Copyirght © Bekam Learning Center</b> All rights reserved</p>
 			</div>
 		</div>
